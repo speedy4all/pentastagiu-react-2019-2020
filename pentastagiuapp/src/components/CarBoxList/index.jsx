237b060d95bae72e7de5722e8c5926a9f486634e
work <work@MacBook-Pro-Vladislav.local>
2020-01-13 22:23:55 +0200

    React Homework

---
+++ pentastagiuapp/src/components/CarBoxList/index.jsx
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import Car from "../Car";
+import "./CarBoxList.css";
+
+export default class CarBoxList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cars: [],
+      carsLoaded: false
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        cars: [
+          {
+            id: 1,
+            name: "Audi",
+            engine: "3.0",
+            combustible: "disel",
+            color: "black",
+            price: "1000"
+          },
+          {
+            id: 2,
+            name: "Bmw",
+            engine: "3.0",
+            combustible: "benzine",
+            color: "blue",
+            price: "1000"
+          },
+          {
+            id: 3,
+            name: "Mercedes",
+            engine: "2.2",
+            combustible: "benzine",
+            color: "silver",
+            price: "1000"
+          },
+          {
+            id: 4,
+            name: "Opel",
+            engine: "2",
+            combustible: "disel",
+            color: "white",
+            price: "1000"
+          },
+          {
+            id: 5,
+            name: "Porsche",
+            engine: "3.0",
+            combustible: "benzine",
+            color: "red",
+            price: "1000"
+          }
+        ],
+        carsLoaded: true
+      });
+    }, 500);
+  }
+
+  renderCarList = () =>
+    this.state.cars.map(car => (
+      <Car
+        key={car.id}
+        name={car.name}
+        engine={car.engine}
+        combustible={car.combustible}
+        color={car.color}
+        price={car.price}
+      />
+    ));
+
+  render() {
+    if (!this.state.carsLoaded)
+      return <div className="loading">Loading...</div>;
+    return <div className="carBoxList">{this.renderCarList()}</div>;
+  }
+}
