872e4e67f81a4fc775a0dc93a506ea35b277e776
Bogdan Mireanu <bogdanmireanu@gmail.com>
2020-01-15 13:11:27 +0200

    Am adaugat key id.

--- tema2/src/App.jsx
+++ tema2/src/App.jsx
@@ -8,24 +8,25 @@ export default class App extends React.Component{
       super(props);
       this.state = {
         phones:[
-          {imageUrl: require('./phones/iphone11.png'), name:'iPHONE 11', price: 5400, stock:'In stock'},
-          {imageUrl: require('./phones/huaweimate30.png'), name:'Huawei Mate 30', price: 3900},
-          {imageUrl: require('./phones/samsungnote10.png'), name:'Samsung Note 10', price: 5899, stock: 'In stock'},
-          {imageUrl: require('./phones/sonyxperia1.png'), name: 'Sony Xperia 1', price:3399},
-          {imageUrl: require('./phones/blackberrykey2.png'), name: 'Blackberry Key2', price: 2980}
+          {id:1, imageUrl: require('./phones/iphone11.png'), name:'iPHONE 11', price: 5400, stock:'In stock'},
+          {id:2, imageUrl: require('./phones/huaweimate30.png'), name:'Huawei Mate 30', price: 3900},
+          {id:3, imageUrl: require('./phones/samsungnote10.png'), name:'Samsung Note 10', price: 5899, stock: 'In stock'},
+          {id: 4, imageUrl: require('./phones/sonyxperia1.png'), name: 'Sony Xperia 1', price:3399},
+          {id: 5, imageUrl: require('./phones/blackberrykey2.png'), name: 'Blackberry Key2', price: 2980}
         ],
         covers:[
-          {imageUrl: require('./covers/coverhuawei.jpg'), name:'Huawei leather cover', price: 120},
-          {imageUrl: require('./covers/coveriphone.jpg'), name:'iPHONE 11 leather cover', price: 140},
-          {imageUrl: require('./covers/coverkey2.jpg'), name:'Blackberry leather cover', price: 99},
-          {imageUrl: require('./covers/covernote10.jpg'), name: 'Note 10 leather cover', price:150, stock:'In stock'},
-          {imageUrl: require('./covers/coverxz2.jpg'), name: 'Sony Xperia leather cover', price: 110, stock: 'In stock'}
+          {id:6, imageUrl: require('./covers/coverhuawei.jpg'), name:'Huawei leather cover', price: 120},
+          {id:7, imageUrl: require('./covers/coveriphone.jpg'), name:'iPHONE 11 leather cover', price: 140},
+          {id:8, imageUrl: require('./covers/coverkey2.jpg'), name:'Blackberry leather cover', price: 99},
+          {id:9, imageUrl: require('./covers/covernote10.jpg'), name: 'Note 10 leather cover', price:150, stock:'In stock'},
+          {id:10, imageUrl: require('./covers/coverxz2.jpg'), name: 'Sony Xperia leather cover', price: 110, stock: 'In stock'}
         ]
       }
    }
 
      renderPhones = () => {
        return  this.state.phones.map(phone => <Phone 
+        key= {phone.id}
         imageUrl={phone.imageUrl}
         name = {phone.name} 
         price = {phone.price} 
@@ -35,6 +36,7 @@ export default class App extends React.Component{
 
      renderCovers = () => {
        return this.state.covers.map(cover => <Cover
+       key= {cover.id}
        imageUrl={cover.imageUrl}
        name = {cover.name}
        price = {cover.price}
