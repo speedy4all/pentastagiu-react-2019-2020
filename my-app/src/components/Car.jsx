b5d3a6714f583b61b8f40030e4f44b1b567ec469
marian-potor <marian.potor@yahoo.com>
2020-01-22 00:16:53 +0200

    tema4 (cart add/remove items )

--- my-app/src/components/Car.jsx
+++ my-app/src/components/Car.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 
 class Car extends React.Component {
+
+    shouldComponentUpdate (nextProps) {
+        return(
+            nextProps.brand !== this.props.brand ||
+            nextProps.fuel !== this.props.fuel ||
+            nextProps.power !== this.props.power ||
+            nextProps.productionYear !== this.props.productionYear ||
+            nextProps.color !== this.props.color ||
+            nextProps.price !== this.props.price 
+        )
+    }
      
     render() {
         return (
@@ -11,6 +22,7 @@ class Car extends React.Component {
                     <td>{ this.props.productionYear?this.props.productionYear:'Not stated' }</td>
                     <td>{ this.props.color?this.props.color:'Not stated' }</td>
                     <td>{ this.props.price?this.props.price:'Not stated' }</td>
+                    <td><button onClick={ () => this.props.addToCart(this.props.id) }>Add to cart</button></td>
                 </tr>
         )
     }
--- my-app/src/components/CartItem.jsx
+++ my-app/src/components/CartItem.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
 class CartItem extends React.Component {
+
+    shouldComponentUpdate (nextProps) {
+        return(
+            nextProps.brand !== this.props.brand ||
+            nextProps.power !== this.props.power ||
+            nextProps.price !== this.props.price ||
+            nextProps.qty !== this.props.qty
+        )
+    }
      
     render() {
         return (
@@ -8,6 +17,13 @@ class CartItem extends React.Component {
                     <td>{ this.props.brand?this.props.brand:'Not stated' }</td>
                     <td>{ this.props.power?this.props.power:'Not stated' }</td>
                     <td>{ this.props.price?this.props.price:'Not stated' }</td>
+                    <td>
+                        <button onClick = {(e) => this.props.newQty(this.props.id, e) } data-direction ="-" >-</button>
+                        { this.props.qty }
+                        <button onClick = {(e) => this.props.newQty(this.props.id, e) } data-direction ="+" >+</button></td>
+                    <td>
+                        <button onClick = { (e) => this.props.removeItem(this.props.id, e) }>X</button>
+                        </td>
                 </tr>
         )
     }
