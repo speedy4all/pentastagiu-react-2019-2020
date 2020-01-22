5adbfec5db05e84b16734e1c1698b4065ebbb5a3
Mihai Voina <mihaivoina@gmail.com>
2020-01-22 11:46:34 +0200

    intermediate update to the homework 4

--- homework/src/components/Cart/cartItem.jsx
+++ homework/src/components/Cart/cartItem.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 export default class CartItem extends React.Component {
 
+    shouldComponentUpdate(nextProps) {
+        return nextProps.brand !== this.props.brand ||
+        nextProps.model !== this.props.model ||
+        nextProps.color !== this.props.color ||
+        nextProps.price !== this.props.price
+    }
+
+
     render() {
         return(
             <div className="cart-item">
@@ -9,6 +17,7 @@ export default class CartItem extends React.Component {
                 <span>Model: {this.props.model}</span>
                 <span>Color: {this.props.color}</span>
                 <span>Price: {this.props.price} Euro</span>
+                <span className="delete-button" onClick={this.props.removeFromCart.bind(this, this.props.cartId)} >&times;</span>
             </div>
         );
     }
