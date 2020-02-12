03fab57e56aaa93a6d5df62dffa490b5b92e18c3
Adrian Ciupe <ciupe_adrian@yahoo.com>
2020-02-12 11:26:35 +0200

    Tema-routing-blog post

---
+++ Tema-routing/src/containers/Blog/Blog.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+// import axios from 'axios';
+import { Route, NavLink, Switch } from 'react-router-dom';
+
+import './Blog.css';
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+
+class Blog extends Component {
+    render () {
+        return (
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><NavLink
+                                to="/posts/"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Posts</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+                
+                <Switch>
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/posts" component={Posts} />
+                </Switch>
+            </div>
+        );
+    }
+}
+
+export default Blog;
---
+++ Tema-routing/src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import axios from '../../../axios';
+import { Route } from 'react-router-dom';
+
+import Post from '../../../components/Post/Post';
+import './Posts.css';
+import FullPost from '../FullPost/FullPost';
+
+class Posts extends Component {
+    state = {
+        posts: []
+    }
+
+    componentDidMount () {
+        console.log( this.props );
+        axios.get( '/posts' )
+            .then( response => {
+                const posts = response.data.slice( 0, 4 );
+                const updatedPosts = posts.map( post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                } );
+                this.setState( { posts: updatedPosts } );
+            } )
+            .catch( error => {
+                console.log( error );
+            } );
+    }
+
+    postSelectedHandler = ( id ) => {
+        this.props.history.push( '/posts/' + id );
+    }
+
+    render () {
+        let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>;
+        if ( !this.state.error ) {
+            posts = this.state.posts.map( post => {
+                return (
+                    <Post
+                        key={post.id}
+                        title={post.title}
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler( post.id )} />
+                );
+            } );
+        }
+
+        return (
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <Route path={this.props.match.url + '/:id'} exact component={FullPost} />
+            </div>
+        );
+    }
+}
+
+export default Posts;
