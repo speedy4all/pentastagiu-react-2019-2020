b67875c7ffac1f3108f94ee76f644e7fc6745023
sdonose <sdonose@pentalog.fr>
2020-02-12 12:11:36 +0200

    router

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,50 @@ import { CartItem } from "./components/CartItem";
 import BillItemList from "./components/BillItemList";
 import { LocaleContext, ThemeContext, AppContext } from "./context";
 import CartList from "./components/CartList";
+import { BrowserRouter as Router, Route, Link, useHistory, Redirect, withRouter } from 'react-router-dom';
+import { createBrowserHistory } from "history";
+import Home from "./components/Home";
+const history = createBrowserHistory();
+function RedirectWithStatus({ from, to, status }) {
+  return (
+    <Route
+      render={({ staticContext }) => {
+        // there is no `staticContext` on the client, so
+        // we need to guard against that here
+        if (staticContext) staticContext.status = status;
+        console.log(staticContext)
+        return <Redirect from={from} to={to} />;
+      }}
+    />
+  );
+}
 
+const Context = React.createContext();
 export default class App extends React.Component {
   static contextType = AppContext;
+
 
   render() {
     if (!this.context.itemLoaded) {
       return <div>Loading...</div>;
     }
+    console.log(this.context)
 
     return (
+      <Router history={history}>
       <LocaleContext.Provider value={{ language: this.context.language }}>
+        <Route exact path="/list" component={BillItemList} />
+        <Route exact path="/home" component={Home} />
+        {/*<RedirectWithStatus status={404} from="/list" to="/" />*/}
         <div className="cart-container">
           <button onClick={this.context.cleanCart}>Remove from cart</button>
           <span>Language: {this.context.language}</span>
           <button onClick={this.context.changeLanguage}>Change language</button>
           <CartList />
         </div>
-        <BillItemList />
+        {/*<BillItemList />*/}
       </LocaleContext.Provider>
+      </Router>
     );
   }
 }
--- src/components/BillItemList/index.jsx
+++ src/components/BillItemList/index.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import BillItem from "../BillItem";
 import { AppContext } from "../../context";
+import { BrowserRouter as Router, Route, Link, useHistory } from 'react-router-dom';
 
 export default class BillItemList extends Component {
   static contextType = AppContext;
 
+  handleClick = () => {
+    this.props.history.push('/')
+  }
   render() {
+    // const history = useHistory();
+     console.log(this.props)
     return (
       <div className="list-container">
         {this.context.itemList.map(item => (
@@ -14,6 +20,9 @@ export default class BillItemList extends Component {
             {...item}
           />
         ))}
+        <button type="button" onClick={this.handleClick}>
+          Go home
+        </button>
       </div>
     );
   }
